8d23923268c9918667a362a2df0edbf348d8999c
jun93713 <jun93713@hotmail.com>
2017-08-02 21:39:46 -0700

    java on the web

--- JavaOOP/Calculator/src/Calculator.java
+++ JavaOOP/Calculator/src/Calculator.java
@@ -1,34 +1,14 @@
+import java.util.ArrayList;
+
 public class Calculator implements java.io.Serializable {
-    private double operandOne;
-    private double operandTwo;
+//    private ArrayList<>
     private double result;
-    private char operation;
+
 
     Calculator(){}
 
-    public void setOperandOne(double operandOne) {
-        this.operandOne = operandOne;
-    }
-
-    public void setOperation(char operation) {
-        this.operation = operation;
-    }
-
-    public void setOperandTwo(double operandTwo) {
-        this.operandTwo = operandTwo;
-    }
-
     public void performOperation(){
-        switch (operation){
-            case '+':
-                result = operandOne + operandTwo;
-                break;
-            case '-':
-                result = operandOne - operandTwo;
-                break;
-            default:
-                break;
-        }
+
     }
 
     public double getResult() {
