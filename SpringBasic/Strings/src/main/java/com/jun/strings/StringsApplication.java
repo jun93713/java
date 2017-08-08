ad6d2b88f1627c132619a53f7628e0282949911f
jun93713 <jun93713@hotmail.com>
2017-08-07 23:33:16 -0700

    Spring!

---
+++ SpringBasic/Strings/src/main/java/com/jun/strings/StringsApplication.java
@@ -0,0 +1,25 @@
+package com.jun.strings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringsApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String index() {
+		return "Hello, client! How are you doing?";
+	}
+	
+	@RequestMapping("/random")
+	public String random() {
+		return "Spring is so cool to use!";
+	}
+}
